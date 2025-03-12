12d890af540dbfe4855b334a61d36129da706e21
Ec <jh1478036@gmail.com>
2025-03-13 04:34:33 +0800

    assignment4_1 commit

---
+++ assignment4/Solution/Project1/Project1/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Project1;
+
+
+namespace Project1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            GenericList<int> intlist = new GenericList<int>();
+            Random random = new Random();
+            for (int x = 0; x < 10; x++)
+            {
+                intlist.Add(random.Next(1, 101)); // 添加随机的数字
+            }
+            //打印
+            intlist.ForEach(x => Console.WriteLine(x));
+            //求和
+            int sum = 0;
+            intlist.ForEach(x => { sum += x; });
+            //求最大值
+            int max = int.MinValue;
+            intlist.ForEach(x => { max = Math.Max(max, x); });
+            //求最小值
+            int min = int.MaxValue;
+            intlist.ForEach(x => { min = Math.Min(min, x); });
+            Console.WriteLine($"Sum:{sum},Max:{max},Min:{min}");
+
+        }
+    }
+}
