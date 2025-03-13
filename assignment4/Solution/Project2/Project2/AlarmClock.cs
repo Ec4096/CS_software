d19573a5e4371058ef1a8e4d449bf578dbba97bb
Ec <jh1478036@gmail.com>
2025-03-13 21:37:09 +0800

    commit_assignment4

---
+++ assignment4/Solution/Project2/Project2/AlarmClock.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Timers;
+
+namespace Clock
+{
+    class AlarmClock
+    {
+        private Timer timer;
+        private DateTime alarmTime;
+
+        public event EventHandler Tick;
+        public event EventHandler Alarm;
+
+        public AlarmClock()
+        {
+            timer = new Timer(1000);
+            timer.Elapsed += TimerElapsed;
+        }
+        public void SetAlarm(DateTime time)
+        {
+            alarmTime = time;
+        }
+        public void Start()
+        {
+            timer.Start();
+        }
+        public void Stop()
+        {
+            timer.Stop();
+        }
+        private void TimerElapsed(object sender, ElapsedEventArgs e)
+        {
+            if (Tick != null)
+            {
+                Tick(this, EventArgs.Empty);
+            }
+
+            if (DateTime.Now >= alarmTime)
+            {
+                if (Alarm != null)
+                {
+                    Alarm(this, EventArgs.Empty);
+                }
+                Stop();
+            }
+        }
+
+    }
+}
---
+++ assignment4/Solution/Project2/Project2/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Timers;
+using Clock;
+
+namespace Project2
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            AlarmClock alarmClock = new AlarmClock();
+            alarmClock.Tick += OnTick;
+            alarmClock.Alarm += OnAlarm;
+
+            alarmClock.SetAlarm(DateTime.Now.AddSeconds(10)); // 设置闹钟在10秒后响铃
+            alarmClock.Start();
+
+            Console.WriteLine("Set a Alarm clock...");
+            Console.ReadKey();
+        }
+
+        private static void OnTick(object sender, EventArgs e)
+        {
+            Console.WriteLine("嘀嗒...");
+        }
+
+        private static void OnAlarm(object sender, EventArgs e)
+        {
+            Console.WriteLine("闹钟响了！");
+        }
+    }
+
+    
+}
+
